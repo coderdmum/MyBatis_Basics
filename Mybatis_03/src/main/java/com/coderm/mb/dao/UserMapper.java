89ee279d1a42c346e189dadfa05c98b48d594c7c
coderm <coderm@163.com>
2021-12-05 15:55:41 +0800

    2021-12-05-1555

--- Mybatis_03/src/main/java/com/coderm/mb/dao/UserMapper.java
+++ Mybatis_03/src/main/java/com/coderm/mb/dao/UserMapper.java
@@ -3,11 +3,15 @@ package com.coderm.mb.dao;
 import com.coderm.mb.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
 
     //根据id查询用户
     User getUserById(int id);
+
+    //分页
+    List<User> getUserLimit(Map<String, Integer> map);
 
 
 }
--- Mybatis_03/src/test/java/com/coderm/mb/dao/UserDaoTest.java
+++ Mybatis_03/src/test/java/com/coderm/mb/dao/UserDaoTest.java
@@ -3,11 +3,16 @@ package com.coderm.mb.dao;
 import com.coderm.mb.pojo.User;
 import com.coderm.mb.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoTest {
+
+    static Logger logger = Logger.getLogger(UserDaoTest.class);
 
     @Test
     public void test() {
@@ -29,7 +34,39 @@ public class UserDaoTest {
         }
     }
 
+    @Test
+    public void log4j() {
+        logger.info("info：进入selectUser方法");
+        logger.debug("debug：进入selectUser方法");
+        logger.error("error: 进入selectUser方法");
 
+    }
+
+
+    @Test
+    public void getUserLimit() {
+        //第一步 ：获取sqlsession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        try{
+            //方式一：执行sql
+            UserMapper userDao = sqlSession.getMapper(UserMapper.class);
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            map.put("startIndex", 1);
+            map.put("pageSize", 2);
+            List<User> list = userDao.getUserLimit(map);
+            //方式二
+//            List<User> userList = sqlSession.selectList("com.coderm.mb.dao.UseMapper.selectUser");
+            for (User user : list){
+                System.out.println(user);
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+    }
 
 }
 
